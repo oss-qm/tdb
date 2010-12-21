2636365fc26db992580fcf811012635f3af32dc1
Jelmer Vernooij <jelmer@debian.org>
2010-12-21 23:39:25 +0100

    Fix dist package builder.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -z "$SAMBA_GIT_URL" ]; then
 	SAMBA_GIT_URL=git://git.samba.org/samba.git
@@ -12,12 +12,11 @@ else
 	git clone --depth 1 $SAMBA_GIT_URL $TDBTMP
 fi
 
-mv $TDBTMP/lib/tdb "tdb-$version"
-mkdir "tdb-$version/lib"
-mv $TDBTMP/buildtools "tdb-$version/buildtools"
-mv $TDBTMP/lib/replace "tdb-$version/lib/replace"
-ln -sf buildtools/scripts/autogen-waf.sh "tdb-$version/autogen-waf.sh"
-rm -rf $TDBTMP
-pushd "tdb-$version" && ./autogen.sh && popd
-tar cvz "tdb-$version" > "tdb_$version.orig.tar.gz"
-rm -rf "tdb-$version"
+pushd $TDBTMP/lib/tdb
+./autogen-waf.sh
+./configure
+make dist
+popd
+version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
+mv $TDBTMP/lib/tdb/tdb-*.tar.gz tdb_$version.orig.tar.gz
+rm -rf $TALLOCTMP
